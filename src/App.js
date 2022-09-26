1e8288ee5cfe0e068acea2dfed52a5910b5bc990
nguyenngocpfievk56 <nguyen.huu.ngoc@elife.co.jp>
2022-09-26 10:16:03 +0700

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          NgocNh edited <code>src/App.js</code> and save to reload.
+          NgocNH - HongBT
         </p>
         <a
           className="App-link"
